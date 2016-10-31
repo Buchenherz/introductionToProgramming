b94aa6e0991b7cbb9bd7b3b673efdcc09cf7da3c
Clemens Pfister <pfistecl@me.com>
2016-10-31 15:28:55 +0100

    added a6

---
+++ Blatt 03/4-pfister-a6.c
@@ -0,0 +1,51 @@
+//
+//  4-pfister-a5.c
+//
+//  Created by Clemens Pfister on 31/10/2016.
+//
+/* 
+ * Lesen Sie vom Benutzer drei Fließkommazahlen 
+ * mittels scanf ein und geben Sie die größte 
+ * und kleinste dieser Zahlen auf dem Bildschirm
+ * aus.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+
+	float firstNumber = 0.0;
+	float secondNumber = 0.0;
+	float thirdNumber = 0.0;
+
+	printf("Enter number 1: ");
+	scanf("%f", &firstNumber);
+	printf("Enter number 2: ");
+	scanf("%f", &secondNumber);
+	printf("Enter number 3: ");
+	scanf("%f", &thirdNumber);
+
+	printf("\n");
+
+	printf("The greatest number you entered is: ");
+	if(firstNumber >= secondNumber && firstNumber >= thirdNumber){
+		printf("%f\n", firstNumber);
+	} else if(secondNumber >= firstNumber && secondNumber >= thirdNumber){
+		printf("%f\n", secondNumber);
+	} else if(thirdNumber >= firstNumber && thirdNumber >= secondNumber){
+		printf("%f\n", thirdNumber);
+	}
+
+	printf("The smallest number you entered is: ");
+	if(firstNumber <= secondNumber && firstNumber <= thirdNumber){
+		printf("%f\n", firstNumber);
+	} else if(secondNumber <= firstNumber && secondNumber <= thirdNumber){
+		printf("%f\n", secondNumber);
+	} else if(thirdNumber <= firstNumber && thirdNumber <= secondNumber){
+		printf("%f\n", thirdNumber);
+	} 
+
+    return EXIT_SUCCESS;
+    
+}
