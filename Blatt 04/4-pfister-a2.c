54f28a0e6b0d6dac24b8768d293be84824daba32
Clemens Pfister <pfistecl@me.com>
2016-11-11 15:37:53 +0100

    not finished a2
    I yet need to get the 0s and 1s of x to work, otherwise good code

--- Blatt 04/4-pfister-a2.c
+++ Blatt 04/4-pfister-a2.c
@@ -6,30 +6,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int main(void){
-
- 	int n = 0;
- 	printf("Enter your number here: ");
- 	scanf("%d", &n);
-
- 	int x, y, z = 0;
-	printf("x, y, z\n");
- 	
- 	for(x = 0; x <= n; x++){
- 		for(y = 0; y < x; y++){
- 			if(y == x){
- 				continue;
- 			}
- 			for(z = 0; z < y; z++){
- 				if(z == y){
- 					continue;
- 				}
- 				printf("%d, %d, %d\n", x, y, z);
-
- 			}
- 		}
- 	}
-
- 	return EXIT_SUCCESS;
- }
+int main(void){
+    
+    int n = 0;
+    printf("Enter your number here: ");
+    scanf("%d", &n);
+    
+    int x, y, z = 0;
+    printf("x, y, z\n");
+    
+    for(x = 0; x <= n; x++){
+        
+        for(y = 0; y < x; y++){
+            if(y == x){
+                continue;
+            }
+            for(z = 0; z < y; z++){
+                if(z == y){
+                    continue;
+                }
+                printf("%d, %d, %d\n", x, y, z);
+                
+            }
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
 
