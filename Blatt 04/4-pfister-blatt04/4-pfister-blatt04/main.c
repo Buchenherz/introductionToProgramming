b1f0e24b7b7c9c4e82e0fa41fa8ef9b614b9cc9c
Clemens Pfister <pfistecl@me.com>
2016-11-14 20:30:28 +0100

    Add Xcode Files
    Warning: messy!

---
+++ Blatt 04/4-pfister-blatt04/4-pfister-blatt04/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  4-pfister-blatt04
+//
+//  Created by Clemens Pfister on 13/11/2016.
+//  Copyright © 2016 Clemens Pfister. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
