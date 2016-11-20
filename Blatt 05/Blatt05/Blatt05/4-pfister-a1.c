228f50ede12950f71ad5f836c4e7a223b058b922
Clemens Pfister <pfistecl@me.com>
2016-11-20 23:41:55 +0100

    added final version of a3, updated a2 and added a1
    Still a lot to fix though

--- Blatt 05/Blatt05/Blatt05/4-pfister-a1.c
+++ Blatt 05/Blatt05/Blatt05/4-pfister-a1.c
@@ -10,29 +10,65 @@
 #include <stdlib.h>
 #include <time.h>
 
-int get_random_num(void){
+int rng(void){
     srand(time(NULL));
     int a = (rand() % 100) + 1;
     printf("%d\n", a);
     return a;
 }
 
-int main() {
+
+int smallest_distance(int a, int b, int c, int num){
     
-    int num = get_random_num();
+    printf("The smallest distance is: ");
     
-    int a, b, c = 0;
-    printf("Please enter your first number: ");
-    scanf("%d", &a);
-    printf("Please enter your second number: ");
-    scanf("%d", &b);
-    printf("Please enter your third number: ");
-    scanf("%d", &c);
+    /* Calculate distances abs() makes value positive if negative */
+    int dist_a = abs(num - a);
+    int dist_b = abs(num - b);
+    int dist_c = abs(num - c);
     
-    if(a == num || b == num || c == num){
-        printf("Congratulations, the correct number was: %d\n", num);
-        return EXIT_SUCCESS;
+    /* Checks for smallest distance between num and a, b, c */
+    if(dist_a <= dist_b && dist_a <= dist_c){
+        printf("%d\n", dist_a);
+    } else if(dist_b <= dist_a && dist_b <= dist_c){
+        printf("%d\n", dist_b);
+    } else if(dist_c <= dist_a && dist_c <= dist_b){
+        printf("%d\n", dist_c);
     }
     
+    return EXIT_SUCCESS;
+}
+
+int main() {
+    /* Get a random number */
+    int num = rng();
     
+    /* While loop that runs until function exit condition is met */
+    while(1 == 1){
+        
+        /* Variable declaration */
+        int a, b, c = 0;
+        printf("Please enter your first number: ");
+        scanf("%d", &a);
+        printf("Please enter your second number: ");
+        scanf("%d", &b);
+        printf("Please enter your third number: ");
+        scanf("%d", &c);
+        
+        /* Check number vs a, b, c */
+        if(a == num || b == num || c == num){
+            printf("Congratulations, the correct number was: %d\n", num);
+            return EXIT_SUCCESS;
+        } else if ((a > num && b > num) || (a > num && c > num) || (b > num && c > num)) {
+            printf("The numbers you choose were for the most part greater than wanted!\n");
+            smallest_distance(a, b, c, num);
+        } else if ((a < num && b < num) || (a < num && c < num) || (b < num && c < num)) {
+            printf("The numbers you choose were for the most part smaller than wanted!\n");
+            smallest_distance(a, b, c, num);
+        } else {
+            /* FIXME: Get equal far away to work */
+            printf("The numbers you choose are equally far away than wanted!\n");
+            smallest_distance(a, b, c, num);
+        }
+    }
 }
