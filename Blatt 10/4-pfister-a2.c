840abc59188cbac119dad54b407aa0d4a8255704
Clemens Pfister <pfistecl@me.com>
2017-01-22 13:00:42 +0100

    Initial commit with source file

---
+++ Blatt 10/4-pfister-a2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Person { 
+	char firstname[64]; 
+	char lastname[64]; 
+	int age; 
+};
+
+int Person_cmp_firstname(const void* x, const void* y){
+
+}
+
+int Person_cmp_lastname(const void* x, const void* y ) {
+
+}
+
+int Person_cmp_age(const void* x, const void* y) {
+
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return EXIT_SUCCESS;
+}
