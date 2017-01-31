a88ef0b34808e250b373db266d407738fca749fd
Clemens Pfister <pfistecl@me.com>
2017-01-31 18:10:41 +0100

    hey

--- VO-Klausur/4-pfister-a1.c
+++ VO-Klausur/4-pfister-a1.c
@@ -2,7 +2,68 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct Boxer{
+	char name[64]; // no name will be longer than 64 chars
+	int wins;
+	double weight;
+} boxer;
+
+boxer inputBoxer(void){
+	boxer boxer = {};
+	printf("Enter boxer name, number of wins, and weight: ");
+	scanf("%s", boxer.name);
+	scanf("%d", &boxer.wins);
+	scanf("%lf", &boxer.weight);
+	return boxer;
+}
+
+void printBoxer(boxer boxer){
+		printf("%s\t%d\t%f\n", boxer.name, boxer.wins, boxer.weight);
+		printf("\n");
+}
+
 int main(int argc, char *argv[]) {
+	int n;
+	printf("Choose number of Boxers: "); scanf("%d", &n);
 	
+	boxer *heavy_boxers = malloc(n * sizeof(boxer));
+	boxer *light_boxers = malloc(n * sizeof(boxer));
+
+	if ((heavy_boxers == NULL) || light_boxers == NULL)	{
+		printf("Error with malloc!\n");
+		return EXIT_FAILURE;
+	}
+	
+	int heavy_counter = 0;
+	int light_counter = 0;
+	
+	for (int i = 0; i < n ;i++) {
+		boxer boxer = inputBoxer();
+		if (boxer.weight >= 100.0) {
+			heavy_boxers[heavy_counter] = boxer;
+			heavy_counter += 1;
+		} else {
+			light_boxers[light_counter] = boxer;
+			light_counter += 1;
+		}
+
+		for (int j = 0;j < heavy_counter;j++) {
+			if (j == 0) {
+				printf("Heavyweights:\nName:\tWins:\tWeight:\n");
+			}
+			printBoxer(heavy_boxers[j]);
+		}
+
+		for (int j = 0;j < light_counter;j++) {
+			if (j == 0) {
+				printf("Lightweights:\nName:\tWins:\tWeight:\n");
+			}
+			printBoxer(light_boxers[j]);
+		}
+	}
+	
+	free(heavy_boxers);
+	free(light_boxers);
 	return EXIT_SUCCESS;
 }
+
--- VO-Klausur/4-pfister-a2.c
+++ VO-Klausur/4-pfister-a2.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+
+char clear(char *input);
+//char mirror(char *input);
+char up(char *input);
 
 int main(int argc, char *argv[]) {
 	
+	char *input = argv[1];
+	char *manipulation = argv[2];
+	// Testing
+	printf("%s, %s\n", input, manipulation);
+	
+	for (int i = 0; i != EOF;i++) {
+	char temp = manipulation[i];
+	if (temp == '\0') 
+		break;
+		
+	if (temp == 'c') {
+		printf("c\n");
+		clear(input);
+		break;
+		
+	} else if (temp == 'm') {
+		//mirror(input);
+		printf("m\n");
+	} else if (temp == 'r') {
+		printf("r\n");
+	} else if (temp == 'u') {
+		up(input);
+		printf("u\n");
+	} else {
+		printf("Not allowed!\n");
+		return EXIT_FAILURE;
+	}
+	
+}
 	return EXIT_SUCCESS;
 }
+
+char clear(char *input){
+	
+	for (int i = 0; input[i] != '\0'; i++) {
+		input[i] = '-';
+	}
+	printf("%s\n", input);
+	return *input;
+}
+
+//char mirror(char *input){
+//	char *input_save = input;
+//	int i = 0;
+//	for (int length = strlen(input); length >= 0;length--) {
+//		output[i] = input_save[length];
+//		i++;
+//	}
+//	
+//	return *input;
+//}
+
+char up(char *input){
+	for (int i = 0;input[i] != '\0';i++) {
+		input[i] += 1;
+	}
+	return *input;
+}
+
